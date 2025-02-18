798df53aceddf1b289e0a6598917e200d1bf4427
namikawayusuke <yu.namikawa18@gmail.com>
2025-02-18 23:31:08 +0900

    fafa

--- src/assets/js/libs/Observer.js
+++ src/assets/js/libs/Observer.js
@@ -1,11 +1,15 @@
 export default class Observer {
-  constructor(target) {
-    this.observer = new IntersectionObserver(this.callback.bind(this), {
+  constructor(target, elem, flag = false, options,) {
+    this.observer = new IntersectionObserver(this.callback.bind(this), options);
+    this.targets = document.querySelectorAll(target);
+    this.elem = elem;
+    this.flag = flag;
+    this.options = {
       root: null,
       rootMargin: "0px",
       threshold: 0.5,
-    });
-    this.targets = document.querySelectorAll(target);
+      ...options  // 外部から渡されたオプションでデフォルト値を上書き
+    };
     // 各要素に対してobserveを実行
     this.targets.forEach(element => {
       this.observer.observe(element);
@@ -15,7 +19,9 @@ export default class Observer {
   callback(entries, observer) {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("is-shown");
+        entry.target.classList.add(this.elem);
+      } else if (this.flag) {
+        entry.target.classList.remove(this.elem);
       }
     });
   }
